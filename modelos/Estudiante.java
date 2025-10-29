05999acbe2b355a444cae5ac604e658999bf9bf6
Josue Martinez <Josueshemuel1405@gmail.com>
2025-10-29 14:15:32 -0500

    Subida de clases Estudiante y Administrador

---
+++ modelos/Estudiante.java
@@ -0,0 +1,14 @@
+public class Estudiante extends Usuario {
+
+    public Estudiante(String usuario, String contrasena) {
+        super(usuario, contrasena);
+    }
+
+    public void solicitarInscripcionCurso(Curso curso) {
+        curso.agregarEstudianteEnEspera(this);
+    }
+
+    public void realizarActividadSumativa(Curso curso, ActividadSumativa ActividadSumativa) {
+        ActividadSumativa.registrarEntrega(this, "La actividad sumativa fue entregada por: " + this.usuario);
+    }
+}
